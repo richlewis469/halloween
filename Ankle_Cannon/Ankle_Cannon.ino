6ad57b0e3f4c73a79d13ca01c27f6e42bec5bf6d
richlewis469 <richlewis469@gmail.com>
2014-09-04 14:06:39 -0500

    Added PIRsensor code for trigger

--- Ankle_Cannon/Ankle_Cannon.ino
+++ Ankle_Cannon/Ankle_Cannon.ino
@@ -1,15 +1,42 @@
 int Relay1 = 2;
 int Relay2 = 3;
+int pirPin = 4;    //the digital pin connected to the PIR sensor's output
+int ledPin = 13;
+
+int calibrationTime = 30;    // PIR Calibration    
+long unsigned int lowIn;         
+
+boolean lockLow = true;
+boolean takeLowTime;  
 
 int relayDelay = 1000;
 
 void setup() 
 { 
+  Serial.begin(9600);
+  // Initialize the Relay
+  Serial.println("setting the relay ");
   pinMode(Relay1, OUTPUT); //Set Pin2 as output 
   pinMode(Relay2, OUTPUT); //Set Pin3 as output 
   digitalWrite(Relay1, HIGH); //Turn off relay 
   digitalWrite(Relay2, HIGH); //Turn off relay
   // Intialize the IR Sensor
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+  for(int i = 0; i < calibrationTime; i++){
+    Serial.print(".");
+    digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+    delay(500);
+    digitalWrite(ledPin, LOW);   //the led visualizes the sensors output pin state
+    delay(500);
+  }
+  Serial.println(" done");
+  Serial.println("SENSOR ACTIVE");
+  delay(50);
 }
 
 void loop() 
@@ -18,9 +45,19 @@ void loop()
   // If someone trips sensor
   // then - Fire cannon, wait 5 seconds
   // else - Loop
-  
-  digitalWrite(Relay1, HIGH); //Turn off relay 
-  delay(relayDelay * 5);
-  digitalWrite(Relay1, LOW); //Turn on relay
-  delay(relayDelay/4);
+  if(digitalRead(pirPin) == HIGH){
+    digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+    Serial.print("motion detected at ");
+    Serial.print(millis()/1000);
+    Serial.println(" sec"); 
+    digitalWrite(Relay1, LOW); //Turn on relay
+    Serial.println("relay on");
+    delay(relayDelay/4);
+    digitalWrite(Relay1, HIGH); //Turn off relay
+    Serial.println("    relay off");
+    digitalWrite(ledPin, LOW);   //the led visualizes the sensors output pin state
+    delay(relayDelay * 5);
+  } else {
+    digitalWrite(Relay1, HIGH); //Turn off relay   
+  }
 } 
